b788b8cd1089beb9e9bbb44d5a4efa254b106695
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-09-28 13:03:48 +0300

    update

---
+++ LCD_Hardware_SPI_Test/Core/Src/lcd.c
@@ -0,0 +1,310 @@
+#include "lcd.h"
+#include "lcd_init.h"
+#include "lcdfont.h"
+#include "stm32f1xx_hal.h"
+
+#define LCD_Buff_Size 1920
+#define LCD_Total_Size 240*280*2
+static unsigned char lcd_buff[LCD_Buff_Size];
+/**********SLEEP IN***************/
+void EnterSleep(void)
+{
+	LCD_WR_REG(0x28);
+	LCD_WR_REG(0x10);
+	HAL_Delay(120);
+}
+
+/**********SLEEP OUT***************/
+void ExitSleep(void)
+{
+	LCD_WR_REG(0x11);
+	HAL_Delay(120);
+	LCD_WR_REG(0x29);
+	HAL_Delay(10);
+}
+
+void LCD_Clear(unsigned int color)
+{          
+	unsigned int i,j; 
+	unsigned char data[2]={0};
+	data[0] = color >> 8;
+	data[1] = color;
+	LCD_Address_Set(0,0,240-1,280-1);//������ʾ��Χ
+	for(j=0;j<LCD_Buff_Size/2;j++)
+	{
+		lcd_buff[j*2] = data[0];
+		lcd_buff[j*2+1] = data[1];
+		
+	}
+	for(i=0;i<LCD_Total_Size/LCD_Buff_Size;i++)
+	{													   	 	
+		LCD_Writ_Bus(lcd_buff,LCD_Buff_Size);
+	} 					  	    
+} 
+
+void LCD_Fill(unsigned int xsta,unsigned int ysta,unsigned int xend,unsigned int yend,unsigned int color)
+{          
+	unsigned int i,j; 
+	LCD_Address_Set(xsta,ysta,xend-1,yend-1);//������ʾ��Χ
+	for(i=ysta;i<yend;i++)
+	{													   	 	
+		for(j=xsta;j<xend;j++)
+		{
+			LCD_WR_DATA(color);
+		}
+	} 					  	    
+}
+
+/******************************************************************************
+      ����˵������ָ��λ�û���
+      ������ݣ�x,y ��������
+                color �����ɫ
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_DrawPoint(unsigned int x,unsigned int y,unsigned int color)
+{
+	LCD_Address_Set(x,y,x,y);//���ù��λ�� 
+	LCD_WR_DATA(color);
+} 
+
+
+/******************************************************************************
+      ����˵��������
+      ������ݣ�x1,y1   ��ʼ����
+                x2,y2   ��ֹ����
+                color   �ߵ���ɫ
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_DrawLine(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2,unsigned int color)
+{
+	unsigned int t; 
+	int xerr=0,yerr=0,delta_x,delta_y,distance;
+	int incx,incy,uRow,uCol;
+	delta_x=x2-x1; //������������ 
+	delta_y=y2-y1;
+	uRow=x1;//�����������
+	uCol=y1;
+	if(delta_x>0)incx=1; //���õ������� 
+	else if (delta_x==0)incx=0;//��ֱ�� 
+	else {incx=-1;delta_x=-delta_x;}
+	if(delta_y>0)incy=1;
+	else if (delta_y==0)incy=0;//ˮƽ�� 
+	else {incy=-1;delta_y=-delta_y;}
+	if(delta_x>delta_y)distance=delta_x; //ѡȡ�������������� 
+	else distance=delta_y;
+	for(t=0;t<distance+1;t++)
+	{
+		LCD_DrawPoint(uRow,uCol,color);//����
+		xerr+=delta_x;
+		yerr+=delta_y;
+		if(xerr>distance)
+		{
+			xerr-=distance;
+			uRow+=incx;
+		}
+		if(yerr>distance)
+		{
+			yerr-=distance;
+			uCol+=incy;
+		}
+	}
+}
+
+
+/******************************************************************************
+      ����˵����������
+      ������ݣ�x1,y1   ��ʼ����
+                x2,y2   ��ֹ����
+                color   ���ε���ɫ
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_DrawRectangle(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,unsigned int color)
+{
+	LCD_DrawLine(x1,y1,x2,y1,color);
+	LCD_DrawLine(x1,y1,x1,y2,color);
+	LCD_DrawLine(x1,y2,x2,y2,color);
+	LCD_DrawLine(x2,y1,x2,y2,color);
+}
+
+
+/******************************************************************************
+      ����˵������Բ
+      ������ݣ�x0,y0   Բ������
+                r       �뾶
+                color   Բ����ɫ
+      ����ֵ��  ��
+******************************************************************************/
+void Draw_Circle(unsigned int x0,unsigned int y0,unsigned char r,unsigned int color)
+{
+	int a,b;
+	a=0;b=r;	  
+	while(a<=b)
+	{
+		LCD_DrawPoint(x0-b,y0-a,color);             //3           
+		LCD_DrawPoint(x0+b,y0-a,color);             //0           
+		LCD_DrawPoint(x0-a,y0+b,color);             //1                
+		LCD_DrawPoint(x0-a,y0-b,color);             //2             
+		LCD_DrawPoint(x0+b,y0+a,color);             //4               
+		LCD_DrawPoint(x0+a,y0-b,color);             //5
+		LCD_DrawPoint(x0+a,y0+b,color);             //6 
+		LCD_DrawPoint(x0-b,y0+a,color);             //7
+		a++;
+		if((a*a+b*b)>(r*r))//�ж�Ҫ���ĵ��Ƿ��Զ
+		{
+			b--;
+		}
+	}
+}
+
+
+/******************************************************************************
+      ����˵������ʾ�����ַ�
+      ������ݣ�x,y��ʾ����
+                num Ҫ��ʾ���ַ�
+                fc �ֵ���ɫ
+                bc �ֵı���ɫ
+                sizey �ֺ�
+                mode:  0�ǵ���ģʽ  1����ģʽ
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_ShowChar(unsigned int x,unsigned int y,unsigned char num,unsigned int fc,unsigned int bc,unsigned char sizey,unsigned char mode)
+{
+	unsigned char temp,sizex,t,m=0;
+	unsigned int i,TypefaceNum;//һ���ַ���ռ�ֽڴ�С
+	unsigned int x0=x;
+	sizex=sizey/2;
+	TypefaceNum=(sizex/8+((sizex%8)?1:0))*sizey;
+	num=num-' ';    //�õ�ƫ�ƺ��ֵ
+	LCD_Address_Set(x,y,x+sizex-1,y+sizey-1);  //���ù��λ�� 
+	for(i=0;i<TypefaceNum;i++)
+	{ 
+		if(sizey==12)temp=ascii_1206[num][i];		       //����6x12����
+		else if(sizey==16)temp=ascii_1608[num][i];		 //����8x16����
+		else if(sizey==24)temp=ascii_2412[num][i];		 //����12x24����
+		else if(sizey==32)temp=ascii_3216[num][i];		 //����16x32����
+		else return;
+		for(t=0;t<8;t++)
+		{
+			if(!mode)//�ǵ���ģʽ
+			{
+				if(temp&(0x01<<t))LCD_WR_DATA(fc);
+				else LCD_WR_DATA(bc);
+				m++;
+				if(m%sizex==0)
+				{
+					m=0;
+					break;
+				}
+			}
+			else//����ģʽ
+			{
+				if(temp&(0x01<<t))LCD_DrawPoint(x,y,fc);//��һ����
+				x++;
+				if((x-x0)==sizex)
+				{
+					x=x0;
+					y++;
+					break;
+				}
+			}
+		}
+	}   	 	  
+}
+
+
+/******************************************************************************
+      ����˵������ʾ�ַ���
+      ������ݣ�x,y��ʾ����
+                *p Ҫ��ʾ���ַ���
+                fc �ֵ���ɫ
+                bc �ֵı���ɫ
+                sizey �ֺ�
+                mode:  0�ǵ���ģʽ  1����ģʽ
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_ShowString(unsigned int x,unsigned int y,const unsigned char *p,unsigned int fc,unsigned int bc,unsigned char sizey,unsigned char mode)
+{         
+	while(*p!='\0')
+	{       
+		LCD_ShowChar(x,y,*p,fc,bc,sizey,mode);
+		x+=sizey/2;
+		p++;
+	}  
+}
+
+
+/******************************************************************************
+      ����˵������ʾ����
+      ������ݣ�m������nָ��
+      ����ֵ��  ��
+******************************************************************************/
+unsigned long mypow(unsigned char m,unsigned char n)
+{
+	unsigned long result=1;	 
+	while(n--)result*=m;
+	return result;
+}
+
+
+/******************************************************************************
+      ����˵������ʾ��������
+      ������ݣ�x,y��ʾ����
+                num Ҫ��ʾ��������
+                len Ҫ��ʾ��λ��
+                fc �ֵ���ɫ
+                bc �ֵı���ɫ
+                sizey �ֺ�
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_ShowIntNum(unsigned int x,unsigned int y,unsigned int num,unsigned char len,unsigned int fc,unsigned int bc,unsigned char sizey)
+{         	
+	unsigned char t,temp;
+	unsigned char enshow=0;
+	unsigned char sizex=sizey/2;
+	for(t=0;t<len;t++)
+	{
+		temp=(num/mypow(10,len-t-1))%10;
+		if(enshow==0&&t<(len-1))
+		{
+			if(temp==0)
+			{
+				LCD_ShowChar(x+t*sizex,y,' ',fc,bc,sizey,0);
+				continue;
+			}else enshow=1; 
+		 	 
+		}
+	 	LCD_ShowChar(x+t*sizex,y,temp+48,fc,bc,sizey,0);
+	}
+} 
+
+
+/******************************************************************************
+      ����˵������ʾ��λС������
+      ������ݣ�x,y��ʾ����
+                num Ҫ��ʾС������
+                len Ҫ��ʾ��λ��
+                fc �ֵ���ɫ
+                bc �ֵı���ɫ
+                sizey �ֺ�
+      ����ֵ��  ��
+******************************************************************************/
+void LCD_ShowFloatNum1(unsigned int x,unsigned int y,float num,unsigned char len,unsigned int fc,unsigned int bc,unsigned char sizey)
+{         	
+	unsigned char t,temp,sizex;
+	unsigned int num1;
+	sizex=sizey/2;
+	num1=num*100;
+	for(t=0;t<len;t++)
+	{
+		temp=(num1/mypow(10,len-t-1))%10;
+		if(t==(len-2))
+		{
+			LCD_ShowChar(x+(len-2)*sizex,y,'.',fc,bc,sizey,0);
+			t++;
+			len+=1;
+		}
+	 	LCD_ShowChar(x+t*sizex,y,temp+48,fc,bc,sizey,0);
+	}
+}
+
+
---
+++ LCD_Hardware_SPI_Test/Core/Src/lcd_init.c
@@ -0,0 +1,300 @@
+#include "lcd_init.h"
+#include "stm32f1xx_hal.h"
+
+extern SPI_HandleTypeDef hspi1;
+void LCD_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0,GPIO_PIN_SET);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1|GPIO_PIN_11, GPIO_PIN_SET);
+}
+
+
+void LCD_Writ_Bus(unsigned char *data, unsigned int size) 
+{	
+	LCD_CS_Clr();
+	while(HAL_SPI_GetState(&hspi1) != HAL_SPI_STATE_READY);
+	HAL_SPI_Transmit(&hspi1, data, size, 0xff);
+	LCD_CS_Set();	
+}
+
+
+void LCD_WR_DATA8(unsigned char data)
+{
+	LCD_Writ_Bus(&data,1);
+}
+
+void LCD_WR_DATA(unsigned int data)
+{
+	unsigned char da[2]= {0};
+	da[0] = data >> 8;
+	da[1] = data;
+	LCD_Writ_Bus(da,2);
+}
+
+
+void LCD_WR_REG(unsigned char data)
+{
+	LCD_DC_Clr();
+	LCD_Writ_Bus(&data,1);
+	LCD_DC_Set();
+}
+
+
+void LCD_Address_Set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2)
+{
+	if(USE_HORIZONTAL==0)
+	{
+		LCD_WR_REG(0x2a);
+		LCD_WR_DATA(x1);
+		LCD_WR_DATA(x2);
+		LCD_WR_REG(0x2b);
+		LCD_WR_DATA(y1+20);
+		LCD_WR_DATA(y2+20);
+		LCD_WR_REG(0x2c);
+	}
+	else if(USE_HORIZONTAL==1)
+	{
+		LCD_WR_REG(0x2a);
+		LCD_WR_DATA(x1);
+		LCD_WR_DATA(x2);
+		LCD_WR_REG(0x2b);
+		LCD_WR_DATA(y1+20);
+		LCD_WR_DATA(y2+20);
+		LCD_WR_REG(0x2c);
+	}
+	else if(USE_HORIZONTAL==2)
+	{
+		LCD_WR_REG(0x2a);//�е�ַ����
+		LCD_WR_DATA(x1+20);
+		LCD_WR_DATA(x2+20);
+		LCD_WR_REG(0x2b);//�е�ַ����
+		LCD_WR_DATA(y1);
+		LCD_WR_DATA(y2);
+		LCD_WR_REG(0x2c);//������д
+	}
+	else
+	{
+		LCD_WR_REG(0x2a);//�е�ַ����
+		LCD_WR_DATA(x1+20);
+		LCD_WR_DATA(x2+20);
+		LCD_WR_REG(0x2b);//�е�ַ����
+		LCD_WR_DATA(y1);
+		LCD_WR_DATA(y2);
+		LCD_WR_REG(0x2c);//������д
+	}
+}
+void LCD_Init(void)
+{
+	LCD_GPIO_Init();
+	LCD_RES_Set();
+	HAL_Delay(10);
+	LCD_RES_Clr();//��λ
+	HAL_Delay(10);
+	LCD_RES_Set();
+	HAL_Delay(120);
+	
+	LCD_BLK_Set();//�򿪱���
+	
+	LCD_WR_REG(0xfd);//private_access
+	LCD_WR_DATA8(0x06);
+	LCD_WR_DATA8(0x08);
+
+	LCD_WR_REG(0x61);//add
+	LCD_WR_DATA8(0x07);//
+	LCD_WR_DATA8(0x04);//
+
+	LCD_WR_REG(0x62);//bias setting
+	LCD_WR_DATA8(0x00);//00
+	LCD_WR_DATA8(0x44);//44
+	LCD_WR_DATA8(0x45);//40  47
+
+	LCD_WR_REG(0x63);//
+	LCD_WR_DATA8(0x41);//
+	LCD_WR_DATA8(0x07);//
+	LCD_WR_DATA8(0x12);//
+	LCD_WR_DATA8(0x12);//
+
+	LCD_WR_REG(0x64);//
+	LCD_WR_DATA8(0x37);//
+	//VSP
+	LCD_WR_REG(0x65);//Pump1=4.7MHz //PUMP1 VSP
+	LCD_WR_DATA8(0x09);//D6-5:pump1_clk[1:0] clamp 28 2b
+	LCD_WR_DATA8(0x10);//6.26
+	LCD_WR_DATA8(0x21);
+	//VSN
+	LCD_WR_REG(0x66); //pump=2 AVCL
+	LCD_WR_DATA8(0x09); //clamp 08 0b 09
+	LCD_WR_DATA8(0x10); //10
+	LCD_WR_DATA8(0x21);
+	//add source_neg_time
+	LCD_WR_REG(0x67);//pump_sel
+	LCD_WR_DATA8(0x20);//21 20
+	LCD_WR_DATA8(0x40);
+
+	//gamma vap/van
+	LCD_WR_REG(0x68);//gamma vap/van
+	LCD_WR_DATA8(0x90);//
+	LCD_WR_DATA8(0x4c);//
+	LCD_WR_DATA8(0x7C);//VCOM  
+	LCD_WR_DATA8(0x66);//
+
+	LCD_WR_REG(0xb1);//frame rate
+	LCD_WR_DATA8(0x0F);//0x0f fr_h[5:0] 0F
+	LCD_WR_DATA8(0x02);//0x02 fr_v[4:0] 02
+	LCD_WR_DATA8(0x01);//0x04 fr_div[2:0] 04
+
+	LCD_WR_REG(0xB4);
+	LCD_WR_DATA8(0x01); //01:1dot 00:column
+	////porch
+	LCD_WR_REG(0xB5);
+	LCD_WR_DATA8(0x02);//0x02 vfp[6:0]
+	LCD_WR_DATA8(0x02);//0x02 vbp[6:0]
+	LCD_WR_DATA8(0x0a);//0x0A hfp[6:0]
+	LCD_WR_DATA8(0x14);//0x14 hbp[6:0]
+
+	LCD_WR_REG(0xB6);
+	LCD_WR_DATA8(0x04);//
+	LCD_WR_DATA8(0x01);//
+	LCD_WR_DATA8(0x9f);//
+	LCD_WR_DATA8(0x00);//
+	LCD_WR_DATA8(0x02);//
+	////gamme sel
+	LCD_WR_REG(0xdf);//
+	LCD_WR_DATA8(0x11);//gofc_gamma_en_sel=1
+	////gamma_test1 A1#_wangly
+	//3030b_gamma_new_
+	//GAMMA---------------------------------/////////////
+
+	//GAMMA---------------------------------/////////////
+	LCD_WR_REG(0xE2);	
+	LCD_WR_DATA8(0x13);//vrp0[5:0]	V0 13
+	LCD_WR_DATA8(0x00);//vrp1[5:0]	V1 
+	LCD_WR_DATA8(0x00);//vrp2[5:0]	V2 
+	LCD_WR_DATA8(0x30);//vrp3[5:0]	V61 
+	LCD_WR_DATA8(0x33);//vrp4[5:0]	V62 
+	LCD_WR_DATA8(0x3f);//vrp5[5:0]	V63
+
+	LCD_WR_REG(0xE5);	
+	LCD_WR_DATA8(0x3f);//vrn0[5:0]	V63
+	LCD_WR_DATA8(0x33);//vrn1[5:0]	V62	
+	LCD_WR_DATA8(0x30);//vrn2[5:0]	V61 
+	LCD_WR_DATA8(0x00);//vrn3[5:0]	V2 
+	LCD_WR_DATA8(0x00);//vrn4[5:0]	V1 
+	LCD_WR_DATA8(0x13);//vrn5[5:0]  V0 13
+
+	LCD_WR_REG(0xE1);	
+	LCD_WR_DATA8(0x00);//prp0[6:0]	V15
+	LCD_WR_DATA8(0x57);//prp1[6:0]	V51 
+
+	LCD_WR_REG(0xE4);	
+	LCD_WR_DATA8(0x58);//prn0[6:0]	V51 
+	LCD_WR_DATA8(0x00);//prn1[6:0]  V15
+
+	LCD_WR_REG(0xE0);
+	LCD_WR_DATA8(0x01);//pkp0[4:0]	V3 
+	LCD_WR_DATA8(0x03);//pkp1[4:0]	V7  
+	LCD_WR_DATA8(0x0d);//pkp2[4:0]	V21
+	LCD_WR_DATA8(0x0e);//pkp3[4:0]	V29 
+	LCD_WR_DATA8(0x0e);//pkp4[4:0]	V37 
+	LCD_WR_DATA8(0x0c);//pkp5[4:0]	V45 
+	LCD_WR_DATA8(0x15);//pkp6[4:0]	V56 
+	LCD_WR_DATA8(0x19);//pkp7[4:0]	V60 
+
+	LCD_WR_REG(0xE3);	
+	LCD_WR_DATA8(0x1a);//pkn0[4:0]	V60 
+	LCD_WR_DATA8(0x16);//pkn1[4:0]	V56 
+	LCD_WR_DATA8(0x0C);//pkn2[4:0]	V45 
+	LCD_WR_DATA8(0x0f);//pkn3[4:0]	V37 
+	LCD_WR_DATA8(0x0e);//pkn4[4:0]	V29 
+	LCD_WR_DATA8(0x0d);//pkn5[4:0]	V21 
+	LCD_WR_DATA8(0x02);//pkn6[4:0]	V7  
+	LCD_WR_DATA8(0x01);//pkn7[4:0]	V3 
+	//GAMMA---------------------------------/////////////
+
+	//source
+	LCD_WR_REG(0xE6);
+	LCD_WR_DATA8(0x00);
+	LCD_WR_DATA8(0xff);//SC_EN_START[7:0] f0
+
+	LCD_WR_REG(0xE7);
+	LCD_WR_DATA8(0x01);//CS_START[3:0] 01
+	LCD_WR_DATA8(0x04);//scdt_inv_sel cs_vp_en
+	LCD_WR_DATA8(0x03);//CS1_WIDTH[7:0] 12
+	LCD_WR_DATA8(0x03);//CS2_WIDTH[7:0] 12
+	LCD_WR_DATA8(0x00);//PREC_START[7:0] 06
+	LCD_WR_DATA8(0x12);//PREC_WIDTH[7:0] 12
+
+	LCD_WR_REG(0xE8); //source
+	LCD_WR_DATA8(0x00); //VCMP_OUT_EN 81-
+	LCD_WR_DATA8(0x70); //chopper_sel[6:4]
+	LCD_WR_DATA8(0x00); //gchopper_sel[6:4] 60
+	////gate
+	LCD_WR_REG(0xEc);
+	LCD_WR_DATA8(0x52);//52
+
+	LCD_WR_REG(0xF1);
+	LCD_WR_DATA8(0x01);//te_pol tem_extend 00 01 03
+	LCD_WR_DATA8(0x01);
+	LCD_WR_DATA8(0x02);
+
+
+	LCD_WR_REG(0xF6);
+	LCD_WR_DATA8(0x09);
+	LCD_WR_DATA8(0x10);
+	LCD_WR_DATA8(0x00);//
+	LCD_WR_DATA8(0x00);//40 3��2ͨ��
+
+	LCD_WR_REG(0xfd);
+	LCD_WR_DATA8(0xfa);
+	LCD_WR_DATA8(0xfc);
+
+	LCD_WR_REG(0x3a);
+	LCD_WR_DATA8(0x05);//
+
+	LCD_WR_REG(0x35);
+	LCD_WR_DATA8(0x00);
+
+	LCD_WR_REG(0x36);
+	if(USE_HORIZONTAL==0)LCD_WR_DATA8(0x08);
+	else if(USE_HORIZONTAL==1)LCD_WR_DATA8(0xC8);
+	else if(USE_HORIZONTAL==2)LCD_WR_DATA8(0x78);
+	else LCD_WR_DATA8(0xA8);
+
+
+	LCD_WR_REG(0x21); 
+
+	LCD_WR_REG(0x11); // exit sleep
+	HAL_Delay(200);
+	LCD_WR_REG(0x29); // display on
+	HAL_Delay(10); 
+} 
+
+
+
+
+
+
+
